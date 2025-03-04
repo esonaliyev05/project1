007effe4f6532766e35e4151f05174c27ac14874
Alyorbekjonxon <alyorbek391@gmail.com>
2025-03-04 23:55:56 +0500

    yangilanish

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./App.css"
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
-    <div className='app_container'>
-     <Header/>
-     <Main/>
+    <div className="app_container">
+      <Header />
+      <Main />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
