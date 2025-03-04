1b1db98396b26226814ce093c9923666b3dff739
Alyorbekjonxon <alyorbek391@gmail.com>
2025-03-04 23:54:59 +0500

    yangilanish

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,58 +1,53 @@
-import React from 'react'
-import "./Main.scss"
+import React from "react";
+import "./Main.scss";
 
 const Main = () => {
   return (
     <main>
-    <div className='main_container'>
-
+      <div className="main_container">
         <div className="main_text">
-            <h1>Projects</h1>
+          <h1>Projects</h1>
 
-            <button>CONTACT ME</button>
+          <button>CONTACT ME</button>
         </div>
 
         <div className="parents-tab">
-  <div className="tab">
-    <img src="public/Bitmap.png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
+          <div className="tab">
+            <img src="public/Bitmap.png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
 
-  <div className="tab">
-    <img src="public/Bitmap (1).png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
+          <div className="tab">
+            <img src="public/Bitmap (1).png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
 
-  <div className="tab">
-    <img src="public/Bitmap (2).png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
-  <div className="tab">
-    <img src="public/Bitmap (3).png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
-  <div className="tab">
-    <img src="public/Bitmap (4).png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
-  <div className="tab">
-    <img src="public/Bitmap (5).png" alt="" />
-    <button>VIEW PROJECT</button>
-    <button>VIEW CODE</button>
-  </div>
-</div>
-
-
-
-
-    </div>
+          <div className="tab">
+            <img src="public/Bitmap (2).png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
+          <div className="tab">
+            <img src="public/Bitmap (3).png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
+          <div className="tab">
+            <img src="public/Bitmap (4).png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
+          <div className="tab">
+            <img src="public/Bitmap (5).png" alt="" />
+            <button>VIEW PROJECT</button>
+            <button>VIEW CODE</button>
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
